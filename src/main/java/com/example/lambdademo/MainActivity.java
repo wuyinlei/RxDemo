0d4c89be925133ac2518587ece40be4a54e16540
unknown <>
2016-04-20 21:24:23 +0000

    加入RxJava的依赖库，简答的使用了RxJava，并且使用RxJava+Lambda表达式简化了部分代码

--- src/main/java/com/example/lambdademo/MainActivity.java
+++ src/main/java/com/example/lambdademo/MainActivity.java
@@ -7,9 +7,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import rx.Observable;
+import rx.Subscriber;
+import rx.functions.Action1;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView mTextView,btu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +48,56 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        mTextView = (TextView) findViewById(R.id.helloRx);
 
+        //常规的观察者和被观察者
+       /* //创建一个被观察者   事件的生产者
+        Observable observable = rx.Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                //添加队列
+                subscriber.onNext("hello rxJava");
+                //完成  还有一个onError()和下面的这个只能在一次的任务中执行一个
+                subscriber.onCompleted();
+            }
+        });
+
+        //创建一个观察者   事件的最终消费者
+        Subscriber<String> subscriber = new Subscriber<String>() {
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(String s) {
+                //完成的时候响应
+                mTextView.setText(s);
+            }
+        };
+
+        //
+        observable.subscribe(subscriber);
+*/
+
+        //上面的代码的简化
+        Observable.just("hello rxJava")
+                .subscribe(new Action1<String>() {
+                    @Override
+                    public void call(String s) {
+                        mTextView.setText(s);
+                    }
+                });
+
+        //利用RxJava和Lambda进一步简化代码
+        Observable.just("hello rxjava and Lambda表达式").subscribe(s->{
+            mTextView.setText(s);
+        });
 
     }
 
