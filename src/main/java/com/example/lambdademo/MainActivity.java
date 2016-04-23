2c6eb938c614c74e37d2a90898b109d3df295c58
wuyinlei <wuyinlei19931118@hotmail.com>
2016-04-23 20:28:59 +0000

    加入Rx的Thread的讲解和Rx的lift()方式，也就是变化的原理

--- src/main/java/com/example/lambdademo/MainActivity.java
+++ src/main/java/com/example/lambdademo/MainActivity.java
@@ -1,19 +1,26 @@
 package com.example.lambdademo;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import rx.Observable;
+import rx.Observer;
 import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int drawables = R.mipmap.ic_launcher;
+    private ImageView mImageView;
 
     private TextView mTextView, btu;
 
@@ -21,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mImageView = (ImageView) findViewById(R.id.image);
 
         //所有不符合lambda语法的都是灰色
       /*  new Thread(new Runnable() {
@@ -98,6 +107,7 @@ public class MainActivity extends AppCompatActivity {
 */
 
      /*   //利用RxJava和Lambda进一步简化代码
+          //just(T...): 将传入的参数依次发送出来
         Observable.just("hello rxjava and Lambda表达式").subscribe(s -> {
             mTextView.setText(s);
         });
@@ -111,8 +121,47 @@ public class MainActivity extends AppCompatActivity {
         observable.observeOn(AndroidSchedulers.mainThread());
 
 
+        //创建了 Observable 和 Observer 之后，再用 subscribe() 方法将它们联结起来，整条链子就可以工作了
         observable.subscribe(mSubscriber);
         observable.subscribe(mSubscriber1);
+
+
+        //
+        String[] names = {"hello", "I", "am", "a", "student"};
+        //from(T[]) / from(Iterable<? extends T>) : 将传入的数组或 Iterable 拆分成具体对象后，依次发送出来。
+        Observable.from(names).subscribe(A -> {
+            Log.d("wuyinlei", A);
+        });
+
+        //这里来加载本地的一张图片
+        Observable.create(new Observable.OnSubscribe<Drawable>() {
+            @Override
+            public void call(Subscriber<? super Drawable> subscriber) {
+                Drawable drawable = getTheme().getDrawable(drawables);
+                subscriber.onNext(drawable);
+                subscriber.onCompleted();
+            }
+            /*那么，加载图片将会发生在 IO 线程，而设置图片则被设定在了主线程。这就意味着，
+            即使加载图片耗费了几十甚至几百毫秒的时间，也不会造成丝毫界面的卡顿。*/
+        }).subscribeOn(Schedulers.io())  //指定 subscribe() 发生在 IO 线程
+                .observeOn(AndroidSchedulers.mainThread())// 指定 Subscriber 的回调发生在主线程
+                .subscribe(new Observer<Drawable>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Toast.makeText(MainActivity.this, "error", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onNext(Drawable drawable) {
+                        mImageView.setImageDrawable(drawable);
+                    }
+                });
+
     }
 
     //被观察者，发送消息时间
@@ -120,26 +169,36 @@ public class MainActivity extends AppCompatActivity {
             Observable.OnSubscribe<String>() {
                 @Override
                 public void call(Subscriber<? super String> subscriber) {
-                   //发送事件
+                    //发送事件
                     subscriber.onNext("hello RxAndroid");
-                   //发送完成的事件
+                    //发送完成的事件
                     subscriber.onCompleted();
                 }
             };
 
 
     //观察者一  接受消息时间，修改TextView控件输出
+
+    //在一个正确运行的事件序列中, onCompleted() 和 onError() 有且只有一个，
+    // 并且是事件序列中的最后一个。需要注意的是，onCompleted() 和 onError()
+    // 二者也是互斥的，即在队列中调用了其中一个，就不应该再调用另一个。
     Subscriber<String> mSubscriber = new Subscriber<String>() {
+
+        //事件队列完结。RxJava 不仅把每个事件单独处理，还会把它们看做一个队列。RxJava 规定，
+        // 当不会再有新的onNext() 发出时，需要触发 onCompleted() 方法作为标志。
         @Override
         public void onCompleted() {
 
         }
 
+        //onError(): 事件队列异常。在事件处理过程中出异常时，onError() 会被触发，
+        // 同时队列自动终止，不允许再有事件发出。
         @Override
         public void onError(Throwable e) {
 
         }
 
+        // RxJava 的事件回调方法除了普通事件 onNext() （相当于 onClick() / onEvent()）
         @Override
         public void onNext(String s) {
             //接受到onNext事件后，就把textview的显示显示为
@@ -166,5 +225,126 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    /**
+     * 在 RxJava 的默认规则中，事件的发出和消费都是在同一个线程的。也就是说，如果只用上
+     * 面的方法，实现出来的只是一个同步的观察者模式。观察者模式本身的目的就是『后台处理，
+     * 前台回调』的异步机制，因此异步对于 RxJava 是至关重要的。而要实现异步，则需要用到
+     * RxJava 的另一个概念： Scheduler 。
+     */
+
+    /**
+     * <p>在RxJava 中，Scheduler ——调度器，相当于线程控制器，RxJava 通过它来指定每一段</p>
+     * <p>代码应该运行在什么样的线程。RxJava 已经内置了几个 Scheduler ，它们已经适合大多</p>
+     * <p>数的使用场景：</p>
+     * <p>Schedulers.immediate(): 直接在当前线程运行，相当于不指定线程。这是默认的 Scheduler。</p>
+     * <p>Schedulers.newThread(): 总是启用新线程，并在新线程执行操作。</p>
+     * <p>Schedulers.io(): I/O 操作（读写文件、读写数据库、网络信息交互等）所使用的 Scheduler。</p>
+     * <p>行为模式和 newThread() 差不多，区别在于 io() 的内部实现是是用一个无数量上限的线程池，可以重用空
+     * <p>闲的线程，因此多数情况下 io() 比 newThread() 更有效率。不要把计算工作放在 io() 中，可以避免创建不必要的线程。</p>
+     * <p>Schedulers.computation(): 计算所使用的 Scheduler。这个计算指的是 CPU 密集型计算，即不会</p>
+     * <p>被 I/O 等操作限制性能的操作，例如图形的计算。这个 Scheduler 使用的固定的线程池，大小为 CPU 核数。不要把 I/O 操作放在 </p>
+     * <p>computation() 中，否则 I/O 操作的等待时间会浪费 CPU。</p>
+     * <p>另外， Android 还有一个专用的 AndroidSchedulers.mainThread()，它指定的操作将在 Android 主线程运行。</p>
+     * <p>有了这几个 Scheduler ，就可以使用 subscribeOn() 和 observeOn() 两个方法来对线程进行控制了。 </p>
+     * <p> subscribeOn(): 指定 subscribe() 所发生的线程，即 Observable.OnSubscribe 被激活时所处的线程。或者叫做事件产生的线程</p>
+     * <p> observeOn(): 指定 Subscriber 所运行在的线程。或者叫做事件消费的线程。</p>
+     */
+
+    //线程测试
+    public void ThreadText() {
+        Observable.just(1, 2, 3, 4)
+                .subscribeOn(Schedulers.io()) // 指定 subscribe() 发生在 IO 线程
+                .observeOn(AndroidSchedulers.mainThread()) // 指定 Subscriber 的回调发生在主线程
+                .subscribe(new Action1<Integer>() {
+                    @Override
+                    public void call(Integer number) {
+                        Log.d("wuyinlei", "number:" + number);
+                    }
+                });
+    }
+
+
+    /**
+     * 这些变换虽然功能各有不同，但实质上都是针对事件序列的处理和再发送。而在 RxJava
+     * 的内部，它们是基于同一个基础的变换方法： lift(Operator)。首先看一下 lift()
+     * 的内部实现（仅核心代码）：
+     * <p>
+     * // 注意：这不是 lift() 的源码，而是将源码中与性能、兼容性、扩展性有关的代码剔除后的核心代码。
+     * // 如果需要看源码，可以去 RxJava 的 GitHub 仓库下载。
+     * public <R> Observable<R> lift(Operator<? extends R, ? super T> operator) {
+     * return Observable.create(new OnSubscribe<R>() {
+     *
+     * @Override public void call(Subscriber subscriber) {
+     * Subscriber newSubscriber = operator.call(subscriber);
+     * newSubscriber.onStart();
+     * onSubscribe.call(newSubscriber);
+     * }
+     * });
+     * }
+     * <p>
+     * 这段代码很有意思：它生成了一个新的 Observable 并返回，而且创建新 Observable 所用的参数
+     * OnSubscribe 的回调方法 call() 中的实现竟然看起来和前面讲过的 Observable.subscribe() 一样！
+     * 然而它们并不一样哟~不一样的地方关键就在于第二行 onSubscribe.call(subscriber) 中的 onSubscribe
+     * 所指代的对象不同（高能预警：接下来的几句话可能会导致身体的严重不适）——
+     * <p>
+     * subscribe() 中这句话的 onSubscribe 指的是 Observable 中的 onSubscribe 对象，这个没有问题，
+     * 但是 lift() 之后的情况就复杂了点。
+     * 当含有 lift() 时：
+     * 1.lift() 创建了一个 Observable 后，加上之前的原始 Observable，已经有两个 Observable 了；
+     * 2.而同样地，新 Observable 里的新 OnSubscribe 加上之前的原始 Observable 中的原始 OnSubscribe，
+     * 也就有了两个 OnSubscribe；
+     * 3.当用户调用经过 lift() 后的 Observable 的 subscribe() 的时候，使用的是 lift() 所返回的新的 Observable
+     * ，于是它所触发的 onSubscribe.call(subscriber)，也是用的新 Observable 中的新 OnSubscribe，即在 lift()
+     * 中生成的那个 OnSubscribe；
+     * 4.而这个新 OnSubscribe 的 call() 方法中的 onSubscribe ，就是指的原始 Observable 中的原始 OnSubscribe ，
+     * 在这个 call() 方法里，新 OnSubscribe 利用 operator.call(subscriber) 生成了一个新的 Subscriber
+     * （Operator 就是在这里，通过自己的 call() 方法将新 Subscriber 和原始 Subscriber 进行关联，并插入自己的
+     * 『变换』代码以实现变换），然后利用这个新 Subscriber 向原始 Observable 进行订阅。
+     * 这样就实现了 lift() 过程，有点像一种代理机制，通过事件拦截和处理实现事件序列的变换。
+     * 精简掉细节的话，也可以这么说：在 Observable 执行了 lift(Operator) 方法之后，会返回一个新的 Observable，
+     * 这个新的 Observable 会像一个代理一样，负责接收原始的 Observable 发出的事件，并在处理后发送给 Subscriber。
+     */
+
+    /*将事件中的 Integer 对象转换成 String 的例子*/
+    public void LiftTest() {
+        Observable observable = Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+
+            }
+        });
+
+        observable.lift(new Observable.Operator<String, Integer>() {
+
+            @Override
+            public Subscriber<? super Integer> call(Subscriber<? super String> subscriber) {
+
+                return new Subscriber<Integer>() {
+                    @Override
+                    public void onCompleted() {
+                        subscriber.onCompleted();
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        subscriber.onError(e);
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        subscriber.onNext("" + integer);
+                    }
+                };
+            }
+        });
+
+        /**
+         * 讲述 lift() 的原理只是为了让你更好地了解 RxJava ，从而可以更好地使用它。
+         * 然而不管你是否理解了 lift() 的原理，RxJava 都不建议开发者自定义 Operator
+         * 来直接使用 lift()，而是建议尽量使用已有的 lift() 包装方法（如 map() flatMap()
+         * 等）进行组合来实现需求，因为直接使用 lift() 非常容易发生一些难以发现的错误。
+         */
+
+    }
 
 }
